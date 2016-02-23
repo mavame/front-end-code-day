de0b6d2a8e67c0787a746c0cd774630a1d0e7602
Matt Van Meter <matt.vanmeter@highlights.com>
2016-02-23 17:34:59 -0500

    My changes

--- dribbble-feed/src/assets/scripts/main.js
+++ dribbble-feed/src/assets/scripts/main.js
@@ -3,6 +3,8 @@
  * @author ?
  */
 
+var jQuery = require('jquery');
+
 // Dribbble API info
 const API_ENDPOINT = 'https://api.dribbble.com/v1/shots/';
 
@@ -41,5 +43,62 @@ function templateFeed(data) {
 
 }
 
-// @example pass data from API response to Handlebars template
-console.log(templateFeed({ items: [] }));
+
+// CUSTOM ADDITIONS
+;(function($) {
+
+	$(DOM.button).click(loadShots);
+
+
+	// Load the shots from the API.
+	function loadShots() {
+
+		$.get(API_ENDPOINT + '?access_token=' + API_TOKEN + '&timeframe=week')
+			.done(function(data) {
+				var defaultSorted = sort(data);
+				renderShots(defaultSorted);
+			});
+	}
+
+
+	/**
+	 * Sort the data by 'views', 'likes', or 'followers' - all ascending order.
+	 * 
+	 * @param  {array} This is an array of shots returned from the API.
+	 * @param  {string} sortBy This is a field to sort by. Options are
+	 *   'views_count', 'likes_count', or 'followers' (all ascending)
+	 * @return {array} This returns an array of sorted shot objects.
+	 */
+	function sort(shots, sortBy) {
+		var sorted = [];
+		if (typeof sortBy === 'undefined') {
+			sortBy = 'views';
+		}
+
+		sorted = shots.sort(function(a, b) {
+			if (a[sortBy] > b[sortBy]) {
+				return -1;
+			} else if (a[sortBy] < b[sortBy]) {
+				return 1;
+			} else {
+				return 0;
+			}
+		});
+
+
+		// @todo implement sortBy == 'likes_count' and sortBy for followers.
+		
+		return sorted;
+
+	}
+
+	/* Render the shots onto the page.
+	 * 
+	 * @param  {array} This is an array of shots returned from the API.
+	 */
+	function renderShots(shots) {
+		DOM.feedContainer.innerHTML = templateFeed({ items: shots });
+	}
+	
+
+})(jQuery);
